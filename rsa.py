c2a36919e726732b86178ba1b1bc675be0f3c281
KobbyBawuah <Bawuah10@hotmail.com>
2022-10-02 01:06:44 -0400

    Created array of coprimes and selected private e

--- rsa.py
+++ rsa.py
@@ -1,17 +1,54 @@
 import sys
+import math
 
 f = open(sys.argv[1],"r")
 contents = f.readlines()
 p, q = 0 , 0
+list = []
+
+def phi (p, q):
+    phi = (p-1)*(q-1)
+    return phi
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def modinv(a, m):
+    for x in range(1, m):
+        if (a * x) % m == 1:
+            return x
+    return None    
+
+def coprimes(a):
+    list = []
+    for x in range(2, a):
+        if gcd(a, x) == 1 and modinv(x,Et) != None:
+            list.append(x)
+    for x in list:
+        if x == modinv(x,Et):
+            list.remove(x)
+    return list
+
 
 for i in contents:
     if " " in i:
-        #set p and q values
+        #read p and q values
         pstring, qstring = i.split()
         p = int(pstring)
         q = int(qstring)
         n = (p * q)
-        print (n)
+
+        #Calculate the Euler totient
+        Et = phi(p, q)
+        #Calculate a list of eligible coprimes of ɸ(n).
+        options = coprimes(Et)
+        #third smallest coprime, e of private key
+        e = options [2]
+
+        print(Et)
+        print (e)
 
     else:
         print (i)
