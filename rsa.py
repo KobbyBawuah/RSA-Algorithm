18e00ba332706008f6e95a5cba6f6829e75e898c
KobbyBawuah <Bawuah10@hotmail.com>
2022-10-01 23:03:55 -0400

    Step 3

--- rsa.py
+++ rsa.py
@@ -1,6 +1,22 @@
 import sys
 
 f = open(sys.argv[1],"r")
-contents = f.read()
-f.close()
-print (contents)
+contents = f.readlines()
+p, q = 0 , 0
+
+for i in contents:
+    if " " in i:
+        #set p and q values
+        pstring, qstring = i.split()
+        p = int(pstring)
+        q = int(qstring)
+        n = (p * q)
+        print (n)
+
+    else:
+        print (i)
+        #do necessary calculations and print statements
+
+
+
+
